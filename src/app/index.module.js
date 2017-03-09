c558a15b3616eed44e5869d01b92e6163abd7da6
wefine <wang.xiaoren@zte.com.cn>
2017-03-09 16:21:50 +0800

    step6, $mdMedia
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,7 +3,9 @@ import angular from "angular";
 import "angular-animate";
 import "angular-aria";
 import "angular-material";
+// css
 import "../public/assets/app.css";
+// business
 import users from "./users/users.module";
 import controller from "./index.controller";
 
@@ -20,6 +22,10 @@ export default angular
         $mdIconProvider
             .defaultIconSet("./assets/svg/avatars.svg", 128)
             .icon("menu", "./assets/svg/menu.svg", 24)
-            .icon("share", "./assets/svg/share.svg", 24);
+            .icon("share", "./assets/svg/share.svg", 24)
+            .icon("google_plus", "./assets/svg/google_plus.svg", 24)
+            .icon("hangouts", "./assets/svg/hangouts.svg", 24)
+            .icon("twitter", "./assets/svg/twitter.svg", 24)
+            .icon("phone", "./assets/svg/phone.svg", 24);
     })
     .controller('AppController', controller);
