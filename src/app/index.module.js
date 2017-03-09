b3540eeb4a979e191986c5364fad439fe20cdfb0
wefine <wang.xiaoren@zte.com.cn>
2017-03-09 16:04:25 +0800

    step5, modify index.html to work with controller, etc.
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,14 +1,11 @@
 // Load libraries
-import angular from 'angular';
-
-import 'angular-animate';
-import 'angular-aria';
-import 'angular-material';
-
-import '../public/assets/app.css';
-
-import users from './users/users.module';
-import controller from './index.controller';
+import angular from "angular";
+import "angular-animate";
+import "angular-aria";
+import "angular-material";
+import "../public/assets/app.css";
+import users from "./users/users.module";
+import controller from "./index.controller";
 
 export default angular
     .module('mdApp', [
@@ -16,9 +13,13 @@ export default angular
         'ui.router',
         users.name
     ])
-    .config(['$locationProvider',
-        function ($locationProvider) {
-            $locationProvider.hashPrefix('!');
-        }
-    ])
+    .config(($mdIconProvider, $locationProvider) => {
+        $locationProvider.hashPrefix('!');
+
+        // Register the user `avatar` icons
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu", "./assets/svg/menu.svg", 24)
+            .icon("share", "./assets/svg/share.svg", 24);
+    })
     .controller('AppController', controller);
